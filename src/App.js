18eff3433c26576ff9a18cfd1b6970d3a2b9ace1
Cobbina Emmanuel <aduboateng333@gmail.com>
2022-11-01 05:51:14 +0000

    main app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./index.css";
+import Profile from "./profile";
+import Links from "./links";
+import Styles from "./styles";
+import Footer from "./footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="app-container">
+      <Profile/>
       </header>
-    </div>
+      <main className="app-container">
+      <Links/>
+      <Styles/>
+      </main>
+      <footer>
+      <Footer/>
+      </footer>
+    </>  
   );
 }
 
